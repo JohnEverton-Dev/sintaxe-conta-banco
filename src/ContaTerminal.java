a1a842396afbddeb11d1ee817ec92bf776523d9b
JohnEverton-Dev <j.everton.ti@gmail.com>
2023-08-31 16:59:12 -0300

    Implementa método Scanner

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws Exception{
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //TODO: Exibir as mensagens para o usuário solicitando os dados da conta.
+        System.out.println("Digite o seu Nome Completo: ");
+        //TODO: Obter os valores digitados no terminal através do Scanner.
+        String nomeCompleto = scanner.nextLine();
+
+        System.out.println("Digite a Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o Numero da Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o seu Saldo: ");
+        Double saldo = scanner.nextDouble();
+
+        //TODO: Retornar a mensagem para o cliente informandos seus dados da conta.
+        System.out.println("PARABÉNS " +nomeCompleto+ "! SEJA BEM VINDO AO NOSSO BANCO!! " +
+                "SUA AGÊNCIA: " +agencia+ " | CONTA: " +numero+
+                " | CONSTA O SALDO DE " +saldo+ " DISPONÍVEL PARA SER UTILIZADO.");
+
+
+
+
+
+
+
     }
+
 }
